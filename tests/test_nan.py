ed52f49cf3259d7e070ecd6730226b84c7debc95
bdehapiot <b.dehapiot@gmail.com>
2023-07-05 11:15:38 +0200

    update

--- tests/test_nan.py
+++ tests/test_nan.py
@@ -3,16 +3,21 @@
 import numpy as np
 from skimage import io 
 from pathlib import Path
-from bdtools import nanfilt
+from bdtools import nanfilt, nanreplace
 
 #%% Initialize ----------------------------------------------------------------
 
 mask = io.imread(Path('resources') / 'nan' / 'mask.tif')
 mask_3d = io.imread(Path('resources') / 'nan' / 'mask_3d.tif')
+mask_zoom = io.imread(Path('resources') / 'nan' / 'mask_zoom.tif')
+mask_3d_zoom = io.imread(Path('resources') / 'nan' / 'mask_3d_zoom.tif')
 noise_nan = io.imread(Path('resources') / 'nan' / 'noise_nan.tif')
 noise_nan_3d = io.imread(Path('resources') / 'nan' / 'noise_nan_3d.tif')
+noise_nan_zoom = io.imread(Path('resources') / 'nan' / 'noise_nan_zoom.tif')
+noise_nan_3d_zoom = io.imread(Path('resources') / 'nan' / 'noise_nan_3d_zoom.tif')
+noise_nan_hole = io.imread(Path('resources') / 'nan' / 'noise_nan_hole.tif')
 
-#%% Test: nanfilt -------------------------------------------------------------
+#%% Test cases ----------------------------------------------------------------
 
 test_cases = [
     
@@ -46,37 +51,173 @@ test_cases = [
         'filt_method': 'mean', 'iterations': 1,
         'parallel': False,
     },
+    {# Test case 05 - 2d image & asymmetric kernel   
+        'img': noise_nan, 'mask': None,
+        'kernel_size': (1, 5), 'kernel_shape': 'cuboid',
+        'filt_method': 'mean', 'iterations': 1,
+        'parallel': False,
+    },
+    {# Test case 06 - 3d image & asymmetric kernel   
+        'img': noise_nan_3d, 'mask': None,
+        'kernel_size': (3, 1, 5), 'kernel_shape': 'cuboid',
+        'filt_method': 'mean', 'iterations': 1,
+        'parallel': False,
+    },
+    {# Test case 07 - 2d image & ellipsoid kernel   
+        'img': noise_nan, 'mask': None,
+        'kernel_size': 3, 'kernel_shape': 'ellipsoid',
+        'filt_method': 'mean', 'iterations': 1,
+        'parallel': False,
+    },
+    {# Test case 08 - 3d image & ellipsoid kernel   
+        'img': noise_nan_3d, 'mask': None,
+        'kernel_size': 3, 'kernel_shape': 'ellipsoid',
+        'filt_method': 'mean', 'iterations': 1,
+        'parallel': False,
+    },
+    {# Test case 09 - 2d image & median filter
+        'img': noise_nan, 'mask': None,
+        'kernel_size': 3, 'kernel_shape': 'cuboid',
+        'filt_method': 'median', 'iterations': 1,
+        'parallel': False,
+    },
+    {# Test case 10 - 2d image & std filter
+        'img': noise_nan, 'mask': None,
+        'kernel_size': 3, 'kernel_shape': 'cuboid',
+        'filt_method': 'std', 'iterations': 1,
+        'parallel': False,
+    },
+    {# Test case 11 - 2d image & multiple iterations
+        'img': noise_nan, 'mask': None,
+        'kernel_size': 3, 'kernel_shape': 'cuboid',
+        'filt_method': 'mean', 'iterations': 3,
+        'parallel': False,
+    },
+    {# Test case 12 - 3d image & parallel processing
+        'img': noise_nan_3d, 'mask': None,
+        'kernel_size': 3, 'kernel_shape': 'cuboid',
+        'filt_method': 'mean', 'iterations': 1,
+        'parallel': True,
+    },
+    {# Test case 13 - 2d zoom image & no mask
+        'img': noise_nan_zoom, 'mask': None,
+        'kernel_size': 3, 'kernel_shape': 'cuboid',
+        'filt_method': 'mean', 'iterations': 1,
+        'parallel': True,
+    },
+    {# Test case 14 - 2d zoom image & mask
+        'img': noise_nan_zoom, 'mask': mask_zoom,
+        'kernel_size': 3, 'kernel_shape': 'cuboid',
+        'filt_method': 'mean', 'iterations': 1,
+        'parallel': True,
+    },
+    {# Test case 15 - 3d zoom image & no mask
+        'img': noise_nan_3d_zoom, 'mask': None,
+        'kernel_size': 3, 'kernel_shape': 'cuboid',
+        'filt_method': 'mean', 'iterations': 1,
+        'parallel': True,
+    },
+    {# Test case 16 - 3d zoom image & mask
+        'img': noise_nan_3d_zoom, 'mask': mask_3d_zoom,
+        'kernel_size': 3, 'kernel_shape': 'cuboid',
+        'filt_method': 'mean', 'iterations': 1,
+        'parallel': True,
+    },    
+    {# Test case 17 (nanreplace only) - 2d hole image & no mask
+        'img': noise_nan_hole, 'mask': None,
+        'kernel_size': 3, 'kernel_shape': 'cuboid',
+        'filt_method': 'mean', 'iterations': 'inf',
+        'parallel': True,
+    },
+    {# Test case 18 (nanreplace only) - 2d hole image & mask
+        'img': noise_nan_hole, 'mask': mask,
+        'kernel_size': 3, 'kernel_shape': 'cuboid',
+        'filt_method': 'mean', 'iterations': 'inf',
+        'parallel': True,
+    },
     
 ]
+
+#%% Test: nanfilt -------------------------------------------------------------
 
 # Load expected outputs
 expected_outputs = []
 for path in Path('resources', 'nan').iterdir():
-    if 'expected' in path.name:
+    if 'nanfilt_expected' in path.name:
         expected_outputs.append(io.imread(path))
 
+# Test cases
+test_outputs = [] 
+for i, case in enumerate(test_cases):
+    if i < 17:
+        try:
+            test_outputs.append(nanfilt(**case))
+        except Exception as e:
+            print(f'nanfilt test case {i:02}: failed')
+            print(f'Error: {e}')
+
+# Compare test & expected outputs
+nanfilt_compare = []
+for i in range(len(test_cases)):
+    if i < 17:
+        compare = np.array_equal(
+            test_outputs[i].astype('float32'), 
+            expected_outputs[i], 
+            equal_nan=True
+            )
+        nanfilt_compare.append(compare)
+        if not compare:
+            print(f'nanfilt test case {i:02}: failed')
+if all(nanfilt_compare):
+    print('nanfilt tests: all passed')
+              
+# -----------------------------------------------------------------------------
+    
+# # Save expected outputs      
+# for i, output in enumerate(test_outputs):
+#     io.imsave(
+#         Path('resources') / 'nan' / f'nanfilt_expected_output_{i:02}.tif',
+#         output.astype('float32'),
+#         check_contrast=False,
+#         )
+
+#%% Test: nanreplace ----------------------------------------------------------
+
 # Load expected outputs
+expected_outputs = []
+for path in Path('resources', 'nan').iterdir():
+    if 'nanreplace_expected' in path.name:
+        expected_outputs.append(io.imread(path))
+
+# Test cases
 test_outputs = [] 
 for i, case in enumerate(test_cases):
     try:
-        test_outputs.append(nanfilt(**case))
+        test_outputs.append(nanreplace(**case))
     except Exception as e:
-        print(f'nanfilt test case {i:02} failed')
+        print(f'nanreplace test case {i:02}: failed')
         print(f'Error: {e}')
 
 # Compare test & expected outputs
+nanreplace_compare = []
 for i in range(len(test_cases)):
-    comparison = np.array_equal(
-        test_outputs[i].astype('float32'), expected_outputs[i], equal_nan=True)
-    if comparison:
-        print(f'nanfilt test case {i:02}: passed')
-    else:
-        print(f'nanfilt test case {i:02}: failed')  
+    compare = np.array_equal(
+        test_outputs[i].astype('float32'), 
+        expected_outputs[i], 
+        equal_nan=True
+        )    
+    nanreplace_compare.append(compare)
+    if not compare:
+        print(f'nanreplace test case {i:02}: failed')
+if all(nanreplace_compare):
+    print('nanreplace tests: all passed')
     
-## Save expected outputs      
+# -----------------------------------------------------------------------------
+    
+# # Save expected outputs      
 # for i, output in enumerate(test_outputs):
 #     io.imsave(
-#         Path('resources') / 'nan' / f'expected_output_{i:02}.tif',
+#         Path('resources') / 'nan' / f'nanreplace_expected_output_{i:02}.tif',
 #         output.astype('float32'),
 #         check_contrast=False,
 #         )
