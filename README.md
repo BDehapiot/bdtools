985ced213595c49de10944fe68e3f90e37e35194
bdehapiot <b.dehapiot@gmail.com>
2023-07-07 12:52:29 +0200

    update

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
+[![Test](https://github.com/BDehapiot/bdtools/actions/workflows/pytest.yml/badge.svg)](https://github.com/BDehapiot/bdtools/actions/workflows/pytest.yml)
+![Python Badge](https://img.shields.io/badge/Python-3.9%20%7C%203.10%20%7C%203.11-blue?logo=python&logoColor=rgb(149%2C157%2C165)&labelColor=rgb(50%2C60%2C65))
+
 # bdtools
 Collection of tools for recurring tasks
-
-[![Test](https://github.com/BDehapiot/bdtools/actions/workflows/pytest.yml/badge.svg)](https://github.com/BDehapiot/bdtools/actions/workflows/pytest.yml)
-
-
-
--- scripts/dynamic_badges.py
+++ scripts/dynamic_badges.py
@@ -1,5 +1,6 @@
 #%% Imports -------------------------------------------------------------------
 
+import urllib.parse
 from pathlib import Path
 
 ROOT_PATH = Path(__file__).resolve().parents[1]
@@ -7,69 +8,42 @@ WORKFLOWS_PATH = ROOT_PATH / '.github' / 'workflows'
 
 #%% 
 
-def extract_list():
+def extract_python_versions():
     
     # Read pytest.yml
     with open(WORKFLOWS_PATH / 'pytest.yml', "r") as file:
         content = file.read()
     
-    # Extract tested OS 
-    idx0 = content.find("os: [") + len("os: [")
-    idx1 = content.find("]", idx0)
-    os_str = content[idx0:idx1]
-    os_list = os_str.split(', ')
-    
-    # # Extract tested Python version(s)
+    # Extract tested Python version(s)
     idx0 = content.find("python-version: [") + len("python-version: [")
     idx1 = content.find("]", idx0)
-    python_str = content[idx0:idx1]
-    python_list = python_str.split(', ')
-    python_list = [item.replace("'", "") for item in python_list]
+    version_str = content[idx0:idx1]
+    version_list = version_str.split(', ')
+    version_list = [item.replace("'", "") for item in version_list]
     
-    return os_list, python_list
+    return version_list
 
-def generate_badge_urls(os_list, python_list):
+def generate_python_badge(version_list):
+    version_list = [version.replace('-', '_') for version in version_list]
+    version_str = ' | '.join(version_list)
+    url_encoded_versions = urllib.parse.quote(version_str)
+    badge_md = f'![Python Badge](https://img.shields.io/badge/Python-{url_encoded_versions}-blue?logo=python&logoColor=rgb(149%2C157%2C165)&labelColor=rgb(50%2C60%2C65))'
     
-    merged_python_str = 
-    
-    python_badge_urls = [
-        f"https://img.shields.io/badge/Python-{item}-blue" 
-        for item in python_list
-        ]
-    os_badge_urls = [
-        f"https://img.shields.io/badge/OS-{item}-blue" 
-        for item in os_list
-        ]
-    
-    return os_badge_urls, python_badge_urls
+    return badge_md
 
-# def update_readme_badges(python_badge_urls):
-    
-#     with open(ROOT_PATH / 'README.md', "a") as file:
-#         file.write("\n\n")
-#         for url in python_badge_urls:
-#             file.write(f"[![Python Version]({url})]({url}) ")
-#         file.write("\n")
-        
-# def update_readme_badges(badge_urls):
-#     formatted_badges = " | ".join(f"[![Badge]({url})]({url})" for url in badge_urls)
-    
-#     with open(ROOT_PATH / 'README.md', "a") as file:
-#         file.write("\n\n")
-#         file.write(formatted_badges)
-#         file.write("\n")
+def update_python_badge(readme_path, badge_md):
+    with open(readme_path, 'r') as file:
+        lines = file.readlines()
 
-# def update_readme_badges(badge_urls):
-#     python_versions_merged = " | ".join(python_versions)
-#     badge_url = f"https://img.shields.io/badge/Python-{python_versions_merged}-blue"
+    for i, line in enumerate(lines):
+        if '![Python Badge]' in line:
+            lines[i] = badge_md + '\n'
 
-#     with open(ROOT_PATH / 'README.md', "a") as file:
-#         file.write("\n\n")
-#         file.write(f"[![Python Versions]({badge_url})]({badge_url})")
-#         file.write("\n")
+    with open(readme_path, 'w') as file:
+        file.writelines(lines)
 
 #%%
 
-os_list, python_list = extract_list()
-os_badge_urls, python_badge_urls = generate_badge_urls(os_list, python_list)
-# update_readme_badges(python_badge_urls)
+version_list = extract_python_versions()
+badge_md = generate_python_badge(version_list)
+update_python_badge(ROOT_PATH / 'README.md', badge_md)
