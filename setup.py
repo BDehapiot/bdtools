1b91549fd9052f7f3a2b5b3a42fd3b6db76c0880
bdehapiot <b.dehapiot@gmail.com>
2025-02-21 15:50:33 +0100

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bd-tools',
-    version='0.2.3',
+    version='0.3.0',
     packages=find_packages(),
     description='Collection of tools for recurring tasks',
     long_description=open('README.md').read(),
